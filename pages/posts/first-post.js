107e841ff51d0527ba32a30539224427e1c5053c
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-02 01:26:55 +0100

    followed docs, nect part is adding the blog posts

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,11 @@
-import Link from 'next/link'
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout from '../../components/layout'
-
 
 export default function FirstPost() {
   return (
-    <>
     <Layout>
-     <Head>
+      <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
@@ -16,7 +14,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-      </Layout>
-    </>
+    </Layout>
   )
 }
